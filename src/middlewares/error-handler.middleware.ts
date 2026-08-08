5237a986a5d283b306ae2d5d1eeb761bb3a74d1b
vishnu <getvishnuvs@gmail.com>
2026-08-08 15:37:03 +0530

    feat: add Error handling middleware

---
+++ src/middlewares/error-handler.middleware.ts
@@ -0,0 +1,26 @@
+import type { NextFunction, Request, Response } from "express";
+import { ZodError } from "zod";
+import { HttpStatus } from "../constants/http.status";
+import { Messages } from "../constants/messages";
+
+export const errorHandler = (
+  error: unknown,
+  _req: Request,
+  res: Response,
+  _next: NextFunction,
+) => {
+  if (error instanceof ZodError) {
+    return res.status(HttpStatus.BAD_REQUEST).json({
+      success: false,
+      message: Messages.VALIDATION.FAILED,
+      errors: error.issues,
+    });
+  }
+
+  console.error(error);
+
+  return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+    success: false,
+    message: Messages.SERVER.INTERNAL_ERROR,
+  });
+};
--- src/middlewares/validate.middleware.ts
+++ src/middlewares/validate.middleware.ts
@@ -4,12 +4,8 @@ import type { ZodSchema } from "zod";
 export const validate = (schema: ZodSchema) => {
   return (req: Request, res: Response, next: NextFunction) => {
     const result = schema.safeParse(req.body);
-
     if (!result.success) {
-      return res.status(400).json({
-        message: "Validation failed",
-        errors: result.error.issues,
-      });
+      return next(result.error);
     }
 
     req.body = result.data;
